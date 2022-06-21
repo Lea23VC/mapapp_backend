9b583ddad4e5ac1dc8656a8947ed5398a976c46c
leandro.villalobos.dx@gmail.com <Lea23VC>
2022-06-21 02:05:47 -0400

    :passport_control: Add permission in Registrer Controller

--- app/Http/Controllers/API/RegisterController.php
+++ app/Http/Controllers/API/RegisterController.php
@@ -98,6 +98,7 @@ class RegisterController extends BaseController
         // You may format this object to suit your needs
         return response()->json([
             'id' => $user->id,
+            'permissions' => $user->permissions()->get(),
             'access_token' => $tokenResult->accessToken,
             'token_type' => 'Bearer',
             'expires_at' => Carbon::parse(
