2c5e008d67f07411064ea5c2eef533c5e330ccd1
leandro.villalobos.dx@gmail.com <Lea23VC>
2022-06-21 19:35:07 -0400

    :art: test
    test

--- app/Http/Controllers/API/MarkerController.php
+++ app/Http/Controllers/API/MarkerController.php
@@ -125,7 +125,8 @@ class MarkerController extends BaseController
                 Log::info($material);
                 $marker->materials()->attach($material);
             }
-            $marker->status()->save(Status::where("code", $input['status'])->first());
+            $status = Status::where("code", $input['status'])->first();
+            $status->marker->save($marker);
 
 
 
