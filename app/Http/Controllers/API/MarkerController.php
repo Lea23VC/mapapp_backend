f22c0033a7f5511fd0ad74f468d76215d44a742b
leandro.villalobos.dx@gmail.com <Lea23VC>
2022-06-22 02:32:47 -0400

    :art: test
    test

--- app/Http/Controllers/API/MarkerController.php
+++ app/Http/Controllers/API/MarkerController.php
@@ -115,21 +115,21 @@ class MarkerController extends BaseController
                 UploadImage::dispatch($path, $filename, $marker->id, $marker);
             }
 
-            if ($request->has("recyclableMaterials")) {
-                $marker->materials()->sync([]);
-                $materials = json_decode($input['recyclableMaterials']);
 
-                foreach ($materials as $materialInput) {
-                    $material = Material::where("code", $materialInput->code)->first();
-                    Log::info("Material: ");
-                    Log::info($material);
-                    Log::info("Material input: ");
-                    Log::info($materialInput->value);
-                    if ($materialInput->value) {
-                        $marker->materials()->syncWithoutDetaching($material);
-                    }
+            $marker->materials()->sync([]);
+            $materials = json_decode($input['recyclableMaterials']);
+
+            foreach ($materials as $materialInput) {
+                $material = Material::where("code", $materialInput->code)->first();
+                Log::info("Material: ");
+                Log::info($material);
+                Log::info("Material input: ");
+                Log::info($materialInput->value);
+                if ($materialInput->value) {
+                    $marker->materials()->syncWithoutDetaching($material);
                 }
             }
+
 
 
             $marker->status()->associate(Status::where("code", $input['status'])->first());
