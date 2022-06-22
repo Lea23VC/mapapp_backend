d3c5992824044ccfcc8e44e24bb3ad108db869d6
leandro.villalobos.dx@gmail.com <Lea23VC>
2022-06-22 11:52:12 -0400

    :art: test
    test

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -19,7 +19,7 @@ class UserResource extends JsonResource
     {
 
 
-        $image = null;
+        $image = 'https://lh5.googleusercontent.com/-b0PKyNuQv5s/AAAAAAAAAAI/AAAAAAAAAAA/AMZuuclxAM4M1SCBGAO7Rp-QP6zgBEUkOQ/s96-c/photo.jpg';
 
         if ($this->imgURL) {
             $expiresAt = new \DateTime('tomorrow');
