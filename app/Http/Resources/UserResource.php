0047daf4dab200a30575155cbf68c2b6897d9b33
leandro.villalobos.dx@gmail.com <Lea23VC>
2022-06-22 00:28:38 -0400

    :lipstick: Add fallback profile image

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -28,7 +28,7 @@ class UserResource extends JsonResource
             if ($imageReference->exists()) {
                 $image = $imageReference->signedUrl($expiresAt);
             } else {
-                $image = null;
+                $image = 'https://lh5.googleusercontent.com/-b0PKyNuQv5s/AAAAAAAAAAI/AAAAAAAAAAA/AMZuuclxAM4M1SCBGAO7Rp-QP6zgBEUkOQ/s96-c/photo.jpg';
             }
         }
 
